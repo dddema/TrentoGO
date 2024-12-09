f21b0d2bfe1d21c478c3677ad5ee479093445db8
gappe22 <andry0powa@gmail.com>
2024-12-09 21:54:12 +0100

    styled App component with tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,22 +3,18 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <RouterView class="font-roboto" />
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import "tailwindcss";
+/* @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'); */
 
-:root {
+/* :root {
   --dark-grey: #2c2c2c;
   --trento-blue: #5171ff;
   --dark-trento-blue: #314dc8;
 
   background-color: #f8f8f8;
-}
-
-* {
-  font-family: 'Roboto', sans-serif;
-  color: var(--color-dark-grey);
-}
+} */
 </style>
