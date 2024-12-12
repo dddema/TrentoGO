73de3eaa2f3586fe425bd12a5d557baae7b427de
gappe22 <andry0powa@gmail.com>
2024-12-12 14:58:02 +0100

    temporary (maybe) removed authLimiter for proxy issues

--- backend/src/api/auth/routes.js
+++ backend/src/api/auth/routes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { googleAuth, login, logout, signup } from './controller.js'
-import { authLimiter, authenticateToken } from './middleware.js'
+import { authenticateToken } from './middleware.js'
 
 const router = express.Router()
 
-router.post('/login', authLimiter, login)
+router.post('/login', login)
 router.get('/logout', authenticateToken, logout)
 router.post('/signup', signup)
 router.post('/google', googleAuth)
