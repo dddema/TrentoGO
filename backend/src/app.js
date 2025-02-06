89b7bc1e2bf291053c525c2201656c6c2bb2a24d
gappe22 <andry0powa@gmail.com>
2025-02-06 01:05:54 +0100

    added users schema and boilerplate code

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+const mongoose = require('mongoose')
+
+async function connectMongoDB() {
+  await mongoose.connect('mongodb+srv://gappe:cacca22@trentogo.vmokg.mongodb.net/?retryWrites=true&w=majority&appName=trentogo')
+}
+
+connectMongoDB().catch(err => console.error(err))
+
+const schemas = require('./schemas')
+const User = mongoose.model('User', schemas.userSchema)
+
+app.get('/', async (req, res) => {
+  // const user = new User({ name: 'chri' })
+  // await user.save()
+
   res.send('Hello World!')
 })
 
