a79801c247b40270d47f7591453e25190d1edc02
gappe22 <andry0powa@gmail.com>
2025-02-10 22:47:25 +0100

    route fix

--- backend/src/api/auth/routes.js
+++ backend/src/api/auth/routes.js
@@ -7,6 +7,6 @@ const router = express.Router()
 router.post('/login', authLimiter, login)
 router.get('/logout', authenticateToken, logout)
 router.post('/signup', signup)
-router.post('/google-auth', googleAuth)
+router.get('/google-auth', googleAuth)
 
 export default router
