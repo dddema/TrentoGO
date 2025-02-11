96cc9f9d00de1b354c45f7a553f1d3fec36f7a61
gappe22 <andry0powa@gmail.com>
2025-02-11 02:23:51 +0100

    fixed (another time XD) google auth

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,6 @@ import RoutesView from '@/views/RoutesView.vue'
 import AuthView from '@/views/AuthView.vue'
 import SignInView from '@/views/SignInView.vue'
 import SignUpView from '@/views/SignUpView.vue'
-import AuthView from '@/views/AuthView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -2,7 +2,10 @@
 import BackButton from "@/components/BackButton.vue";
 import RoundButton from "@/components/RoundButton.vue";
 import { GoogleLogin } from "vue3-google-login";
+import axios from "axios";
 // import GoogleIcon from "@/assets/icons/GoogleIcon.svg";
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 const signUp = () => {
 }
@@ -12,7 +15,7 @@ const logIn = () => {
 
 const googleSignIn = async (response) => {
   try {
-    const url = 'http://localhost:3000/auth/google'
+    const url = 'http://trentogo.onrender.com/auth/google'
     const res = await axios.post(url, {
       idToken: response.credential
     })
@@ -30,7 +33,7 @@ const googleSignIn = async (response) => {
     
     <div class="h-full flex flex-col justify-end pb-5 items-center">
       <!-- <RoundButton @button-click="googleSignIn" text="Continua con Google" color="bg-trento-white" text-color="text-dark-gray" text-weight="font-medium" hover-color="hover:bg-dark-gray/5" :icon="GoogleIcon"/> -->
-      <GoogleLogin :callback="googleSignIn" prompt auto-login />
+      <GoogleLogin :client-id="GOOGLE_CLIENT_ID" :callback="googleSignIn" prompt auto-login  />
       
       <div class="flex items-center w-full my-5">
         <hr class="flex-grow border-t border-light-gray">
